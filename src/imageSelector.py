f12048b73c6a0277d80735208fbf7c559515b7df
MissingDriver <callmeboby@gmail.com>
2019-04-08 19:41:28 -0500

    Just adding some stuff to allow for image selection and saving

---
+++ src/imageSelector.py
@@ -0,0 +1,23 @@
+from tkinter import Tk
+from PIL import Image
+from PIL import ImageTk
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
+import numpy as np
+import cv2
+
+def selectImage():
+    Tk().withdraw()
+    filename = askopenfilename(filetypes=(("jpeg","*.jpg"),("bmp","*.bmp"),("png","*.png"))) #lets you open jpg, bmp, or png images
+    return(cv2.imread(filename))
+
+def cv2ToPIL(cv2Image):#Converts a cv2 np.array into PIL format
+    image = cv2.cvtColor(cv2Image, cv2.COLOR_BGR2RGB) #CV2 uses BGR format instead of RGB so you have to put them in the write order
+    return(Image.fromarray(image)) 
+
+def saveImage(image):
+    if type(image) is np.ndarray:   #checks to see if the image is an np array
+        image = cv2ToPIL(image)
+    file = asksaveasfilename(defaultextension=".jpg", filetypes=(("jpeg","*.jpg"),("bmp","*.bmp"),("png","*.png"))) #lets you save images as jpg, bmp, or png
+    if file:
+        image.save(file)
