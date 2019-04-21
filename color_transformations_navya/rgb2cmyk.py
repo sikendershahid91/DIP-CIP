51874d6f668af4ab1c5dd64f2005bfb2e9978b11
NavyaSushmaTummala <tummalanavyasushma08@gmail.com>
2019-04-20 21:03:41 -0500

    sky is input of rgb to cmyk

---
+++ color_transformations_navya/rgb2cmyk.py
@@ -0,0 +1,38 @@
+# Author : Navya Sushma Tummala
+# rgb to cmyk
+import numpy as np
+import cv2
+# reading an image
+image = cv2.imread("sky.png")
+[rows, cols, dim] = np.shape(image)
+cmyk = np.zeros((rows, cols, 4), dtype="uint8")
+for i in range(rows):
+    for j in range(cols):
+        # dividing by 255
+        blue = image[i, j, 0] / 255
+        green = image[i, j, 1] / 255
+        red = image[i, j, 2] / 255
+        # getting cmy components
+        c = 1 - red
+        m = 1 - green
+        y = 1 - blue
+        # extracting min of cmy to find k - black channel
+        cmy_min = np.minimum(np.minimum(c, m), y)
+        if cmy_min != 1:
+            temp = 1 - cmy_min
+            range_cmyk = 100
+            # c, m, y, k channels
+            cmyk[i][j][0] = range_cmyk * (c - cmy_min) / temp
+            cmyk[i][j][1] = range_cmyk * (m - cmy_min) / temp
+            cmyk[i][j][2] = range_cmyk * (y - cmy_min) / temp
+            cmyk[i][j][3] = range_cmyk * cmy_min
+        else:
+            cmyk[i][j][0] = 0
+            cmyk[i][j][1] = 0
+            cmyk[i][j][2] = 0
+            cmyk[i][j][3] = 1
+cv2.imshow('result', cmyk)
+cv2.waitKey(0)
+
+
+
