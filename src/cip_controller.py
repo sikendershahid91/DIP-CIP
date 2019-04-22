ee2596db0920ccbe952fc76a349620b4f4c19811
sikender shahid <sikendershahid91@gmail.com>
2019-04-21 20:02:45 -0700

    update directory

--- src/cip_controller.py
+++ src/cip_controller.py
@@ -1,3 +1,26 @@
+#import imageSelector
+import gui
+import filters
+import color_model
+import cip
+import time
+import numpy as np
+import cv2
+    
+"""
+test = imageSelector.image_selector
+test.selectImage()
+a = imageSelector.image_selector
+
+cv2.imshow('image',a.cv2Image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
+
+#gui.app.window
+gui.app.window.protocol("WM_DELETE_WINDOW", gui.app.window.quit)
+gui.app.window.mainloop()
+
 from tkinter import Tk
 from PIL import Image
 from PIL import ImageTk
